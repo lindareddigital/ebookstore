4eb5bc3820494f1aeb50c33b226e984e8e2a309d
wonski chan <wonski.chan@vicosys.com.hk>
2024-03-28 11:27:56 +0800

    Add Error Handling For get Product By Series

--- src/pages/api/product/series.js
+++ src/pages/api/product/series.js
@@ -1,22 +1,22 @@
 import apiManager from "src/pages/api/api";
 
 export default async function handler(req, res) {
-
   try {
-
     console.log("category", req.body.category_id);
 
-    const arr = req.body.category_id
+    // Check if req.body.series is empty or not an array
+    if (!req.body.series || !Array.isArray(req.body.series) || req.body.series.length === 0) {
+      return res.status(400).json({ error: "Invalid series data provided" });
+    }
 
-
+    const arr = req.body.series;
 
     const result = await apiManager.getProductBySeries(arr);
 
-    
-
     res.status(200).json({ result });
   } catch (err) {
-    console.log("series error ", err);
+    console.error("Error:", err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 }
 
