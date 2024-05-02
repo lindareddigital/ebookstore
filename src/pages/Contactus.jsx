e13a5b14e0f805575555f31369ca73ee6361f510
y75434 <linda.lin@red-digital.com>
2024-05-02 16:05:57 +0800

    新增表單驗證

--- src/pages/Contactus.jsx
+++ src/pages/Contactus.jsx
@@ -3,35 +3,34 @@ import MenuBar from "src/pages/components/molecules/MenuBar";
 import Breadcrumb from "src/pages/components/molecules/Breadcrumb";
 import { useEffect, useState } from "react";
 import Head from "next/head";
-import { z } from "zod";
+import Toast from "react-bootstrap/Toast";
+
 import {
   createDirectus,
   rest,
-  utilsImport,
   staticToken,
   uploadFiles,
 } from "@directus/sdk";
-import apiManager from "src/pages/api/api";
-import { Directus } from "@directus/sdk";
-
-
-export const signUpFormSchema = z.object({
-  email: z.string().email({ message: "錯誤的信箱格式" }),
-  // password: z.string().min(8, {
-  //   message: "密碼最短需要8個字符",
-  // }),
-});
 
 export default function ContactUs() {
   const [formData, setFormData] = useState({
     email: "",
-    // password: "",
+    title: "",
+    fullname: "",
+    content:"",
   });
+  const [showToast, setShowToast] = useState(false);
   const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    if (localStorage.getItem("email") == null) {
+      location.replace(`/login`);
+    }
+  }, []);
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    const formData = new FormData(event.currentTarget);
+    const formData = new FormData(event.currentTarget);     
 
     const email = formData.get("email");
     const phone = formData.get("phone");
@@ -40,56 +39,39 @@ export default function ContactUs() {
     const type = formData.get("type");
     const content = formData.get("content");
 
-    try {
-      // 验证表单数据
-      signUpFormSchema.parse(formData);
-
-      // 如果数据验证通过，这里可以执行提交逻辑
-      console.log("Form data:", formData);
-    } catch (error) {
-      // 如果数据验证失败，更新错误状态
-      if (error instanceof z.ZodError) {
-        const fieldErrors = {};
-        error.errors.forEach((err) => {
-          const fieldName = err.path.join(".");
-          fieldErrors[fieldName] = err.message;
-        });
-        setErrors(fieldErrors);
-      } else {
-        console.error("Validation error:", error);
-      }
-    }
-
     const client = createDirectus("https://directus-cms.vicosys.com.hk")
       .with(rest())
       .with(staticToken(process.env.NEXT_PUBLIC_TOKEN));
 
     const fileInput = document.querySelector("#formFile");
 
-    const picData = new FormData();
-    for (const file of fileInput.files) {
-      picData.append("files", file);
+    // 有檔案才上傳
+    if (fileInput.files.length > 0) {
+      const picData = new FormData();
+      for (const file of fileInput.files) {
+        picData.append("files", file);
+      }
+      const result = await client.request(uploadFiles(picData));
+      console.log("result", result);
+
+      let ids = [];
+
+      if (Array.isArray(result)) {
+        ids = result.map((item) => item.id);
+      } else if (typeof result === "object" && result.id) {
+        ids.push(result.id);
+      }
+
+      console.log("ids:", ids);
+
+      var transformedArray = ids.map((id) => ({
+        directus_files_id: {
+          id: id,
+        },
+      }));
     }
-    const result = await client.request(uploadFiles(picData));
-    console.log("result", result);
 
-     let ids = [];
-
-     if (Array.isArray(result)) {
-       ids = result.map((item) => item.id);
-     } else if (typeof result === "object" && result.id) {
-       ids.push(result.id);
-     }
-
-     console.log("ids:", ids);
-
-     const transformedArray = ids.map((id) => ({
-       directus_files_id: {
-         id: id,
-       },
-     }));
-
-     console.log(transformedArray);
+    //  console.log(transformedArray);
 
     let token = localStorage.getItem("token");
 
@@ -113,21 +95,24 @@ export default function ContactUs() {
     );
 
     const data = await response.json();
-    console.log("data", data.data.id);
-
+    console.log("pass form success", data.data.id);
+    setShowToast(true);
     const id = data.data.id
 
-    const patchForm = await fetch("/api/patchForm", {
-      method: "PATCH",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ 
-        token,
-        id,
-        transformedArray 
-      }),
-    });
+    // 有檔案才把檔案放進form
+    if (fileInput.files.length > 0) {
+      const patchForm = await fetch("/api/patchForm", {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ 
+          token,
+          id,
+          transformedArray 
+        }),
+      });
 
-    console.log("patchForm", patchForm);
+      console.log("patchForm success", patchForm);
+    }
     
   };
 
@@ -137,6 +122,34 @@ export default function ContactUs() {
       ...prevFormData,
       [name]: value,
     }));
+
+    const errors = validateForm(formData);
+    if (Object.keys(errors).length > 0) {
+      setErrors(errors);
+    }
+  };
+
+  const validateForm = (data) => {
+    const errors = {};
+    if (!data.email) {
+      errors.email = "郵件地址不能為空";
+    } else if (!isValidEmail(data.email)) {
+      errors.email = "請輸入有效的郵件地址";
+    }
+    if (!data.fullname) {
+      errors.fullname = "姓名不能為空";
+    }
+    if (!data.title) {
+      errors.title = "主旨不能為空";
+    }
+    if (!data.content) {
+      errors.content = "內容不能為空";
+    }
+    return errors;
+  };
+
+  const isValidEmail = (email) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
   };
 
   return (
@@ -147,6 +160,22 @@ export default function ContactUs() {
       <Navbar />
       <MenuBar />
       <Breadcrumb data={"聯絡我們"} />
+      <Toast
+        onClose={() => setShowToast(false)}
+        show={showToast}
+        // delay={3000}
+        // autohide
+        style={{
+          position: "absolute",
+          top: 200,
+          right: 20,
+        }}
+      >
+        <Toast.Header>
+        </Toast.Header>
+        <Toast.Body>表單傳送成功！</Toast.Body>
+      </Toast>
+
       <form onSubmit={onSubmit} className="form-area contact-us">
         <div className="block-title">聯絡我們</div>
         <div className="red-word">*必須填寫</div>
@@ -160,8 +189,12 @@ export default function ContactUs() {
             className="form-control"
             id="fullname"
             aria-describedby="fullname"
+            value={formData.fullname}
+            onChange={handleChange}
           ></input>
         </div>
+        {errors.fullname && <div className="red-word">{errors.fullname}</div>}
+
         <div className="">
           <label htmlFor="nickname" className="form-label">
             暱稱
@@ -186,30 +219,19 @@ export default function ContactUs() {
             value={formData.email}
             onChange={handleChange}
           ></input>
-          {/* {errors.email && <div className="error-message">{errors.email}</div>} */}
         </div>
-        <div className="">
-          <label htmlFor="phone" className="form-label">
-            phone <span className="red-word">*</span>
-          </label>
-          <input
-            name="phone"
-            type="text"
-            className="form-control"
-            id="phone"
-            aria-describedby="phone"
-          ></input>
-        </div>
+        {errors.email && <div className="red-word">{errors.email}</div>}
+
         <div className="">
           <label className="" htmlFor="floatingSelect">
-            主旨
+            類型 <span className="red-word">*</span>
           </label>
 
           <select
             name="type"
             className="form-select"
             id="floatingSelect"
-            aria-label="Floating label select example"
+            aria-label="Floating label select"
           >
             <option value="1.書籍意見">1.書籍意見</option>
             <option value="2.異業合作">2.異業合作</option>
@@ -222,6 +244,22 @@ export default function ContactUs() {
         </div>
 
         <div className="">
+          <label htmlFor="title" className="form-label">
+            主旨 <span className="red-word">*</span>
+          </label>
+          <input
+            name="title"
+            type="text"
+            className="form-control"
+            id="title"
+            aria-describedby="title"
+            value={formData.title}
+            onChange={handleChange}
+          ></input>
+        </div>
+        {errors.title && <div className="red-word">{errors.title}</div>}
+
+        <div className="">
           <label className="" htmlFor="content">
             內容<span className="red-word">*</span>
             <div className="sub-word">(字數150字以內)</div>
@@ -229,10 +267,12 @@ export default function ContactUs() {
           <textarea
             name="content"
             className="form-control form-comments"
-            placeholder="Leave a comment here"
             id="content"
+            value={formData.content}
+            onChange={handleChange}
           ></textarea>
         </div>
+        {errors.content && <div className="red-word">{errors.content}</div>}
 
         <div className="">
           <label htmlFor="formFile" className="form-label">
@@ -245,14 +285,12 @@ export default function ContactUs() {
             accept=".docx, .doc, .jpg, .jpeg, .png"
             type="file"
             id="formFile"
+            placeholder="請上傳 docx.doc.jpg.jpeg.png 格式"
             multiple
           ></input>
         </div>
         <div className="button-group">
-          <button
-            type="submit"
-            className="btn cancel-btn info-site-btn"
-          >
+          <button type="submit" className="btn cancel-btn info-site-btn">
             取消
           </button>
           <button type="submit" className="btn info-site-btn">
